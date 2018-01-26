965e9caf49cb5b190d846b157332ded131d73389
kibook <annadaniau@gmail.com>
2018-01-26 11:34:36 -0500

    Read RPC info from defaults

--- s1kd-newdml/s1kd-newdml.c
+++ s1kd-newdml/s1kd-newdml.c
@@ -409,6 +409,10 @@ void copy_default_value(const char *def_key, const char *def_val)
 		strcpy(security_classification, def_val);
 	else if (strcmp(def_key, "brex") == 0 && strcmp(brex_dmcode, "") == 0)
 		strcpy(brex_dmcode, def_val);
+	else if (strcmp(def_key, "responsiblePartnerCompany") == 0 && strcmp(defaultRpcName, "") == 0)
+		strcpy(defaultRpcName, def_val);
+	else if (strcmp(def_key, "responsiblePartnerCompanyCode") == 0 && strcmp(defaultRpcCode, "") == 0)
+		strcpy(defaultRpcCode, def_val);
 }
 
 void show_help(void)
@@ -517,7 +521,6 @@ void set_issue_date(xmlNodePtr issueDate)
 	xmlSetProp(issueDate, BAD_CAST "year",  BAD_CAST year_s);
 	xmlSetProp(issueDate, BAD_CAST "month", BAD_CAST month_s);
 	xmlSetProp(issueDate, BAD_CAST "day",   BAD_CAST day_s);
-
 }
 
 int main(int argc, char **argv)
