1c0b2e19c7200108ad8f886d19e757d9e582cad9
kibook <annadaniau@gmail.com>
2018-07-09 11:33:15 -0400

    s1kd-brexcheck: Bump version

--- tools/s1kd-brexcheck/s1kd-brexcheck.c
+++ tools/s1kd-brexcheck/s1kd-brexcheck.c
@@ -27,7 +27,7 @@
 #define XSI_URI BAD_CAST "http://www.w3.org/2001/XMLSchema-instance"
 
 #define PROG_NAME "s1kd-brexcheck"
-#define VERSION "1.2.0"
+#define VERSION "1.2.1"
 
 #define E_PREFIX PROG_NAME ": ERROR: "
 #define F_PREFIX PROG_NAME ": FAILED: "
