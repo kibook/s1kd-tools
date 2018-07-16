ee287746d063b60113ca0f469ae8768ed128e0b8
kibook <annadaniau@gmail.com>
2018-07-16 10:26:13 -0400

    s1kd-brexcheck: Fix invalid read

--- tools/s1kd-brexcheck/s1kd-brexcheck.c
+++ tools/s1kd-brexcheck/s1kd-brexcheck.c
@@ -27,7 +27,7 @@
 #define XSI_URI BAD_CAST "http://www.w3.org/2001/XMLSchema-instance"
 
 #define PROG_NAME "s1kd-brexcheck"
-#define VERSION "1.2.2"
+#define VERSION "1.2.3"
 
 #define E_PREFIX PROG_NAME ": ERROR: "
 #define F_PREFIX PROG_NAME ": FAILED: "
@@ -648,6 +648,7 @@ int check_brex_rules(xmlDocPtr brex_doc, xmlNodeSetPtr rules, xmlDocPtr doc, con
 
 			if (!(severity = xmlGetProp(rules->nodeTab[i], BAD_CAST "brSeverityLevel"))) {
 				severity = defaultBrSeverityLevel;
+				defaultBrSeverityLevel = NULL;
 			}
 
 			brexError = xmlNewChild(brexCheck, NULL, BAD_CAST "brexError",
@@ -694,6 +695,7 @@ int check_brex_rules(xmlDocPtr brex_doc, xmlNodeSetPtr rules, xmlDocPtr doc, con
 		xmlFree(use);
 	}
 
+	xmlFree(defaultBrSeverityLevel);
 	xmlXPathFreeContext(context);
 
 	return nerr;
