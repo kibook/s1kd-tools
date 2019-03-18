3754d050f07cfd571891ddccf10d893dcaf6aec2
kibukj <kibuzhj@gmail.com>
2019-03-18 11:35:37 -0400

    Handle out-of-context errors in s1kd-validate
    Set the structured error message handler for messages without a
    parsing/validating context.

--- tools/s1kd-validate/s1kd-validate.c
+++ tools/s1kd-validate/s1kd-validate.c
@@ -8,7 +8,7 @@
 #include <libxml/xinclude.h>
 
 #define PROG_NAME "s1kd-validate"
-#define VERSION "1.2.0"
+#define VERSION "1.2.1"
 
 #define ERR_PREFIX PROG_NAME ": ERROR: "
 #define SUCCESS_PREFIX PROG_NAME ": SUCCESS: "
@@ -454,6 +454,8 @@ int main(int argc, char *argv[])
 	if (verbosity == SILENT) {
 		schema_errfunc = suppress_error;
 	}
+
+	xmlSetStructuredErrorFunc(stderr, schema_errfunc);
 
 	if (optind < argc) {
 		for (i = optind; i < argc; ++i) {
