c693ecdd1ec5170498d6bb8b169a06106a2c480d
kibukj <kibuzhj@gmail.com>
2019-03-21 12:21:04 -0400

    Bump version of s1kd-uom

--- tools/s1kd-uom/s1kd-uom.c
+++ tools/s1kd-uom/s1kd-uom.c
@@ -9,7 +9,7 @@
 #include "uom.h"
 
 #define PROG_NAME "s1kd-uom"
-#define VERSION "1.2.2"
+#define VERSION "1.3.0"
 
 #define ERR_PREFIX PROG_NAME ": ERROR: "
 #define WRN_PREFIX PROG_NAME ": WARNING: "
