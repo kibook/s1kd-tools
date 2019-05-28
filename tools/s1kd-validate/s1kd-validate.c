f0398dbd10af99b5dd4029dec17ff0b00b91cf26
kibukj <kibuzhj@gmail.com>
2019-05-28 10:24:17 -0400

    Fix xrefid test

--- tools/s1kd-validate/s1kd-validate.c
+++ tools/s1kd-validate/s1kd-validate.c
@@ -7,7 +7,7 @@
 #include "s1kd_tools.h"
 
 #define PROG_NAME "s1kd-validate"
-#define VERSION "2.1.0"
+#define VERSION "2.1.1"
 
 #define ERR_PREFIX PROG_NAME ": ERROR: "
 #define SUCCESS_PREFIX PROG_NAME ": SUCCESS: "
@@ -35,7 +35,7 @@
 	"//@nextActionRefId[not(//@id=.)]|" \
 	"//@refapplic[not(//@id=.)]|" \
 	"//@refid[not(//@id=.)]|" \
-	"//@xrefid[not(//id=.)]"
+	"//@xrefid[not(//@id=.)]"
 
 #define INVALID_IDS_XPATH BAD_CAST \
 	"//@reasonForUpdateRefIds|//@warningRefs|//@cautionRefs"
