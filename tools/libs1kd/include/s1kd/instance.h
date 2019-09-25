1f03f4773f7f004bd4dd973872a6dd9c79215a69
kibukj <kibuzhj@gmail.com>
2019-09-25 10:26:37 -0400

    Add doxygen comments to libs1kd headers

--- tools/libs1kd/include/s1kd/instance.h
+++ tools/libs1kd/include/s1kd/instance.h
@@ -1,9 +1,21 @@
+/**
+ * @file instance.h
+ * @brief Create instances of S1000D CSDB objects
+ */
+
 #ifndef S1KD_INSTANCE
 #define S1KD_INSTANCE
 
 #include <stdbool.h>
 #include <libxml/tree.h>
 
+/**
+ * Create a filtered instance based on user-defined applicability.
+ *
+ * @param doc The CSDB object
+ * @param defs Applicability definitions to filter on
+ * @param reduce Whether or not to hide redundant applicability
+ */
 void s1kdFilter(xmlDocPtr doc, xmlNodePtr defs, bool reduce);
 
 #endif
--- tools/libs1kd/include/s1kd/metadata.h
+++ tools/libs1kd/include/s1kd/metadata.h
@@ -1,10 +1,31 @@
+/**
+ * @file metadata.h
+ * @brief Retrieve and set metadata on CSDB objects
+ */
+
 #ifndef S1KD_METADATA
 #define S1KD_METADATA
 
 #include <libxml/xmlstring.h>
 #include <libxml/tree.h>
 
+/**
+ * Retrieve metadata from a CSDB object.
+ *
+ * @param doc The CSDB object
+ * @param name Name of the metadata
+ * @return A new xmlChar * containing the value of the metadata
+ */
 xmlChar *s1kdGetMetadata(xmlDocPtr doc, const xmlChar *name);
+
+/**
+ * Set metadata in a CSDB object.
+ *
+ * @param doc The CSDB object
+ * @param name Name of the metadata
+ * @param value New value of the metadata
+ * @return 0 if successful, non-zero otherwise
+ */
 int s1kdSetMetadata(xmlDocPtr doc, const xmlChar *name, const xmlChar *value);
 
 #endif
