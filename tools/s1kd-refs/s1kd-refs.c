34cc40174f84428f8566fc72b816cf1ec9556bc0
kibukj <kibuzhj@gmail.com>
2019-11-22 12:02:03 -0500

    s1kd-refs: Bump version

--- tools/s1kd-refs/s1kd-refs.c
+++ tools/s1kd-refs/s1kd-refs.c
@@ -13,7 +13,7 @@
 #include "s1kd_tools.h"
 
 #define PROG_NAME "s1kd-refs"
-#define VERSION "4.3.0"
+#define VERSION "4.4.0"
 
 #define ERR_PREFIX PROG_NAME ": ERROR: "
 #define SUCC_PREFIX PROG_NAME ": SUCCESS: "
