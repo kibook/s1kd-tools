ffa784f9079f9014193027d8208f9b3835b18f8c
kibukj <kibuzhj@gmail.com>
2020-02-05 13:26:06 -0500

    Show all leaks in valgrind test

--- tools/libs1kd/tests/run.sh
+++ tools/libs1kd/tests/run.sh
@@ -3,4 +3,4 @@
 make -C .. all
 make all
 
-LD_LIBRARY_PATH=.. valgrind --leak-check=full --error-exitcode=1 ./tests
+LD_LIBRARY_PATH=.. valgrind --leak-check=full --show-leak-kinds=all --error-exitcode=1 ./tests
