531bcf8604033c321bc9cc1a48ef304d829fc326
kibukj <kibuzhj@gmail.com>
2020-05-07 13:24:53 -0400

    libs1kd: Update tests

--- tools/libs1kd/tests/tests.c
+++ tools/libs1kd/tests/tests.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <string.h>
 #include <libxml/tree.h>
 #include <s1kd/brexcheck.h>
 #include <s1kd/instance.h>
@@ -45,52 +46,74 @@ void test_metadata(void)
 	xmlDocPtr doc = xmlReadFile("test.xml", NULL, 0);
 	xmlChar *date;
 
-	date = s1kdGetMetadata(doc, BAD_CAST "issueDate");
+	date = s1kdDocGetMetadata(doc, BAD_CAST "issueDate");
 	printf("DATE: %s\n", (char *) date);
 	xmlFree(date);
 
-	s1kdSetMetadata(doc, BAD_CAST "issueDate", BAD_CAST "1970-01-01");
+	s1kdDocSetMetadata(doc, BAD_CAST "issueDate", BAD_CAST "1970-01-01");
 
-	date = s1kdGetMetadata(doc, BAD_CAST "issueDate");
+	date = s1kdDocGetMetadata(doc, BAD_CAST "issueDate");
 	printf("DATE: %s\n", (char *) date);
 	xmlFree(date);
 
 	xmlFreeDoc(doc);
+}
+
+void test_metadata_2(void)
+{
+	char *xml;
+	char *date;
+	char *result;
+	int size;
+
+	xml = "<issueDate year=\"2020\" month=\"05\" day=\"01\"/>";
+
+	date = s1kdGetMetadata(xml, strlen(xml), "issueDate");
+	printf("DATE: %s\n", (char *) date);
+	free(date);
+
+	s1kdSetMetadata(xml, strlen(xml), "issueDate", "1970-01-01", &result, &size);
+
+	date = s1kdGetMetadata(result, size, "issueDate");
+	printf("DATE: %s\n", (char *) date);
+	free(date);
+
+	free(result);
 }
 
 void test_instance(void)
 {
 	xmlDocPtr doc = xmlReadFile("test.xml", NULL, 0);
 	xmlDocPtr out;
-	s1kdApplicDefs defs = s1kdNewApplicDefs();
+	s1kdApplicability app = s1kdNewApplicability();
 	char *result;
 	int size;
 
-	s1kdAssign(defs, BAD_CAST "version", BAD_CAST "prodattr", BAD_CAST "A");
+	s1kdAssign(app, BAD_CAST "version", BAD_CAST "prodattr", BAD_CAST "A");
 
-	out = s1kdDocFilter(doc, defs, S1KD_FILTER_REDUCE);
+	out = s1kdDocFilter(doc, app, S1KD_FILTER_REDUCE);
 
 	xmlSaveFile("-", out);
 
-	s1kdFreeApplicDefs(defs);
+	s1kdFreeApplicability(app);
 	xmlFreeDoc(doc);
 	xmlFreeDoc(out);
 }
 
 void test_instance_2(void)
 {
-	s1kdApplicDefs defs = s1kdNewApplicDefs();
+	s1kdApplicability app = s1kdNewApplicability();
 	char *result;
 	int size;
 
-	s1kdAssign(defs, BAD_CAST "version", BAD_CAST "prodattr", BAD_CAST "A");
+	s1kdAssign(app, BAD_CAST "version", BAD_CAST "prodattr", BAD_CAST "A");
 
-	s1kdFilter("<root/>", 7, defs, S1KD_FILTER_DEFAULT, &result, &size);
+	s1kdFilter("<root/>", 7, app, S1KD_FILTER_DEFAULT, &result, &size);
 
 	puts(result);
 
 	free(result);
-	s1kdFreeApplicDefs(defs);
+	s1kdFreeApplicability(app);
 }
 
 int main()
@@ -98,6 +121,7 @@ int main()
 	test_brexcheck();
 	test_brexcheck_2();
 	test_metadata();
+	test_metadata_2();
 	test_instance();
 	test_instance_2();
 
