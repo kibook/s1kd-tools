be064c003cb51a100172eed1c37ec54074014891
kibook <kibuzhj@gmail.com>
2024-04-03 15:58:44 +0000

    Fix build-mingw.sh

--- build-mingw.sh
+++ build-mingw.sh
@@ -5,4 +5,4 @@ out=build/$MSYSTEM
 make
 mkdir -p "$out"
 cp tools/s1kd-*/s1kd-*.exe "$out"
-ldd "$out"/s1kd-*.exe | awk '{print $3}' | grep '^/mingw64' | sort -u | xargs cp -t "$out"
+ldd "$out"/s1kd-*.exe | awk '{print $3}' | grep '^/mingw' | sort -u | xargs cp -t "$out"
