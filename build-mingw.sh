845702f52684ab29c18e8cb252bc760c3a33de11
kibook <kibuzhj@gmail.com>
2025-05-23 07:38:53 -0400

    Clean make targets in MinGW build script

--- build-mingw.sh
+++ build-mingw.sh
@@ -7,6 +7,6 @@ out=build/$MSYSTEM
 pacman --noconfirm -S --needed $(printf "${MINGW_PACKAGE_PREFIX}-%s " gcc make pkgconf libxml2 libxslt libsystre) vim
 
 mkdir -p "$out"
-make -j$(nproc)
+make -j$(nproc) clean all
 cp tools/*/*.exe "$out"
 ldd "$out"/*.exe | awk '{print $3}' | grep '^/mingw' | sort -u | xargs -r cp -t "$out"
